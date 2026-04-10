ac55f2df9fe38510290d8d2b1c034971746fd34f
Chythanya-v <chythanya.v27@gmail.com>
2026-04-10 06:00:44 +0530

    added cors before routes

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-app.use(express.json());
-app.use("/", rootRouter)
-
 app.use(cors({
-    origin: 'https://task-manager-kappa-woad.vercel.app/', // your actual Vercel URL
+    origin: 'https://task-manager-kappa-woad.vercel.app',
     credentials: true
 }))
+
+app.use(express.json());
+app.use("/", rootRouter)
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
